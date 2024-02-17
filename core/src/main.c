b5e2abab24076a301fb5d0379ca2bec059385de3
Thiago Prado <thiagopereiraprado@gmail.com>
2024-02-17 11:48:42 -0300

    Use HAL to configure clock

--- core/src/main.c
+++ core/src/main.c
@@ -6,10 +6,12 @@
 #include "core_cm3.h"
 
 #include "buzzer.h"
-#include "rcc.h"
+
 #include "timer.h"
 
 #include "led_strip.h"
+
+#include "stm32f1xx_hal.h"
 
 /** Definitions --------------------------------------------------- */
 /**
@@ -52,6 +54,7 @@ static buzzer_note_t sheet_music[] = {
 /** Prototypes ---------------------------------------------------- */
 static void timer_callback(void);
 static bool timer_check_timeout(uint32_t timeshot, uint32_t timeout);
+static void clock_config(void);
 
 /** Internal functions -------------------------------------------- */
 /**
@@ -73,13 +76,41 @@ static bool timer_check_timeout(uint32_t timeshot, uint32_t timeout) {
     return false;
 }
 
+/**
+ * @brief MCU clock configuration.
+ */
+static void clock_config(void)
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+    HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+    /** Initializes the CPU, AHB and APB buses clocks
+     */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
+}
+
 /** Public functions ---------------------------------------------- */
 int main(void) {
     uint32_t led_update_timeshot = 0;
     uint32_t buzzer_timeshot = 0;
     uint32_t note_idx = 0;
 
-    rcc_clock_init();
+    HAL_Init();
+    clock_config();
 
     buzzer_setup();
 
